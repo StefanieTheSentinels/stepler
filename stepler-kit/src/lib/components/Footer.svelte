02f793f23e762d8b1a36508e772db8b16ab0018e
StefanieTheSentinels <maximovich2006@gmail.com>
2026-01-09 22:55:17 +0300

    Footer+Header

---
+++ stepler-kit/src/lib/components/Footer.svelte
@@ -0,0 +1,62 @@
+<footer class="text-[#5C5C5C]">
+  <div class="mx-auto w-[1070px] border-t-2 border-[#5C5C5C]">
+    <div class="w-full  py-10">
+    <div class="flex flex-row items-start gap-12">
+      <!-- 1 -->
+      <div class="flex flex-col gap-8 text-[14px] leading-[1.78] max-w-[258px]">
+        <div class="flex flex-col gap-4">
+          <div class="text-[21px] font-bold tracking-[0.1em] text-black mb-4.5">STEPLER</div>
+          <div class="h-[2px] w-[262px] bg-[#5C5C5C]"></div>
+        </div>
+        <div class="flex flex-col gap-4 leading-[1.34]">
+        <div>
+            Лицензия на образовательную<br>
+            деятельность<br>
+            №Л035-01298-77/00273709 от<br>
+            13.10.2015
+          </div>
+          <div class="font-bold leading-[1.78]">
+            © <span class="font-normal">2024-2025 Все права защищены</span>
+          </div>
+          <a class="inline-flex leading-[1.05] underline underline-offset-4 hover:opacity-80" href="/">
+            Политика обработки персональных данных
+          </a>
+        </div>
+      </div>
+      <div class="flex w-full flex-1 flex-row justify-evenly gap-0">
+        <!-- 2 -->
+        <div class="flex flex-col gap-4 text-[14px] leading-[1.78]">
+          <div class="font-semibold text-[#4A4A4A]">Полезные ссылки:</div>
+          <div class="flex flex-col gap-2">
+            <a class="hover:underline hover:underline-offset-4" href="/">Нормативные документы</a>
+            <a class="hover:underline hover:underline-offset-4" href="/">Список курсов</a>
+          </div>
+        </div>
+        <!-- 3 -->
+        <div class="flex flex-col gap-4 text-[14px] leading-[1.78]">
+          <div class="font-semibold text-[#4A4A4A]">Контакты:</div>
+          <div class="flex flex-col gap-2">
+            <a class="underline underline-offset-4 hover:opacity-80" href="/">Почта</a>
+            <a class="underline underline-offset-4 hover:opacity-80" href="/">Номер телефона</a>
+            <div>+7 499 213 05 37</div>
+          </div>
+        </div>
+      </div>
+      <!-- 4 -->
+      <div class="flex flex-col gap-4 text-[14px] leading-[1.78] max-w-[253px]">
+        <div class="font-semibold text-[#4A4A4A]">Юр. информация:</div>
+        <div class="flex flex-col gap-2">
+          <div>ООО СКФКС</div>
+          <div>ИНН 5074095180</div>
+          <div>КПП 507401001</div>
+          <div>ОГРН 1255000066518</div>
+        </div>
+        <div class="pt-4">
+          <div>Московская область, г. Подольск,</div>
+          <div>цл. Циолковского, д. 3а</div>
+        </div>
+      </div>
+      </div>
+    </div>
+  </div>
+</footer>
--- stepler-kit/src/routes/+layout.svelte
+++ stepler-kit/src/routes/+layout.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { page } from '$app/stores';
+	import Header from '$lib/components/Header.svelte';
+  	import Footer from '$lib/components/Footer.svelte';
 
 	let { children } = $props();
 </script>
 
+
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+<div class="mx-auto w-full max-w-[1157px] bg-white">
+	<Header />
+	{@render children()}
+
+	<Footer />
+</div>
