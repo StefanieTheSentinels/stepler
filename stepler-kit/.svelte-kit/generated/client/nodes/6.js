0fd1066124c676fdd0a235db53a23f9f632d0836
StefanieTheSentinels <maximovich2006@gmail.com>
2026-01-13 11:51:00 +0300

    Remove duplicate gitignore in stepler-kit

---
+++ stepler-kit/.svelte-kit/generated/client/nodes/6.js
@@ -0,0 +1,1 @@
+export { default as component } from "../../../../src/routes/extra(RENAME)/+page.svelte";
---
+++ stepler-kit/.svelte-kit/non-ambient.d.ts
@@ -0,0 +1,45 @@
+
+// this file is generated — do not edit it
+
+
+declare module "svelte/elements" {
+	export interface HTMLAttributes<T> {
+		'data-sveltekit-keepfocus'?: true | '' | 'off' | undefined | null;
+		'data-sveltekit-noscroll'?: true | '' | 'off' | undefined | null;
+		'data-sveltekit-preload-code'?:
+			| true
+			| ''
+			| 'eager'
+			| 'viewport'
+			| 'hover'
+			| 'tap'
+			| 'off'
+			| undefined
+			| null;
+		'data-sveltekit-preload-data'?: true | '' | 'hover' | 'tap' | 'off' | undefined | null;
+		'data-sveltekit-reload'?: true | '' | 'off' | undefined | null;
+		'data-sveltekit-replacestate'?: true | '' | 'off' | undefined | null;
+	}
+}
+
+export {};
+
+
+declare module "$app/types" {
+	export interface AppTypes {
+		RouteId(): "/" | "/auth" | "/blog" | "/courses" | "/extra(RENAME)";
+		RouteParams(): {
+			
+		};
+		LayoutParams(): {
+			"/": Record<string, never>;
+			"/auth": Record<string, never>;
+			"/blog": Record<string, never>;
+			"/courses": Record<string, never>;
+			"/extra(RENAME)": Record<string, never>
+		};
+		Pathname(): "/" | "/auth" | "/auth/" | "/blog" | "/blog/" | "/courses" | "/courses/" | "/extra(RENAME)" | "/extra(RENAME)/";
+		ResolvedPathname(): `${"" | `/${string}`}${ReturnType<AppTypes['Pathname']>}`;
+		Asset(): "/robots.txt" | string & {};
+	}
+}
